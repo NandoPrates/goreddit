7732aa0f066efd69ef1b101d45f783942fe71c25
Jefferson <jefferson.dab@gmail.com>
2016-11-05 19:09:59 -0300

    Formatting code with gofmt

--- links.go
+++ links.go
@@ -1,42 +1,42 @@
 package goreddit
 
 import (
-  "net/url"
+	"net/url"
 )
 
 // SubmitText submits a text (markdown format) to a subreddit
 func (r *Reddit) SubmitText(sub string, title string, text string) error {
-  return r.Submit(sub, title, text, "self")
+	return r.Submit(sub, title, text, "self")
 }
 
 // SubmitLink submits a new link to the specified subreddit
 func (r *Reddit) SubmitLink(sub string, title string, url string) error {
-  return r.Submit(sub, title, url, "link")
+	return r.Submit(sub, title, url, "link")
 }
 
 // Submit is the general purpose function to submit a new thread to Reddit
 // it can be either a link or a text (self) thread. This shouldn't be called directly
 // use SubmitLink or SubmitText instead.
 func (r *Reddit) Submit(sub string, title string, content string, kind string) (err error) {
-  form := url.Values{
-    "api_type": {"json"},
-    "sr": {sub},
-    "kind": {kind},
-    "title": {title},
-    "resubmit": {"true"},
-  }
+	form := url.Values{
+		"api_type": {"json"},
+		"sr":       {sub},
+		"kind":     {kind},
+		"title":    {title},
+		"resubmit": {"true"},
+	}
 
-  if kind == "self" {
-    form.Add("text", content)
-  } else {
-    form.Add("url", content)
-  }
+	if kind == "self" {
+		form.Add("text", content)
+	} else {
+		form.Add("url", content)
+	}
 
-  request,err := r.Request("POST", "/api/submit", form)
-  if err != nil {
-    return
-  }
+	request, err := r.Request("POST", "/api/submit", form)
+	if err != nil {
+		return
+	}
 
-  _,err = r.JsonResponse(request)
-  return
+	_, err = r.JsonResponse(request)
+	return
 }
--- users.go
+++ users.go
@@ -1,66 +1,66 @@
 package goreddit
 
-import(
-  // "fmt"
-  // "io/ioutil"
-  "encoding/json"
-  // "net/url"
+import (
+	// "fmt"
+	// "io/ioutil"
+	"encoding/json"
+	// "net/url"
 )
 
 type User struct {
-  Username string `json:"name"`
-  IsFriend bool `json:"is_friend"`
-  Created float64 `json:"created"`
-  HiddenFromRobots bool `json:"hidden_from_robots"`
-  LinkKarma int `json:"link_karma"`
-  CommentKarma int `json:"comment_karma"`
-  Gold bool `json:"is_gold"`
-  Mod bool `json:"is_mod"`
-  Verified bool `json:"has_verified_email"`
-  Id string `json:"id"`
+	Username         string  `json:"name"`
+	IsFriend         bool    `json:"is_friend"`
+	Created          float64 `json:"created"`
+	HiddenFromRobots bool    `json:"hidden_from_robots"`
+	LinkKarma        int     `json:"link_karma"`
+	CommentKarma     int     `json:"comment_karma"`
+	Gold             bool    `json:"is_gold"`
+	Mod              bool    `json:"is_mod"`
+	Verified         bool    `json:"has_verified_email"`
+	Id               string  `json:"id"`
 }
 
 type Me struct {
-  User
-  Over18 bool `json:"over_18"`
-  Suspended bool `json:"is_suspended"`
-  Employee bool `json:"is_employee"`
+	User
+	Over18    bool `json:"over_18"`
+	Suspended bool `json:"is_suspended"`
+	Employee  bool `json:"is_employee"`
 }
 
 // User returns a *User with the available information to an username
 func (r *Reddit) User(username string) (user *User, err error) {
-  request,err := r.Request("GET", "/user/"+username+"/about", nil)
-  if err != nil {
-    return
-  }
-  
-  data,err := r.JsonResponse(request)
-  if err != nil {
-    return
-  }
+	request, err := r.Request("GET", "/user/"+username+"/about", nil)
+	if err != nil {
+		return
+	}
 
-  var obj struct { Data *User }
-  err = json.Unmarshal(data, &obj)
-  if err != nil {
-    return
-  }
+	data, err := r.JsonResponse(request)
+	if err != nil {
+		return
+	}
 
-  user = obj.Data
-  return
+	var obj struct{ Data *User }
+	err = json.Unmarshal(data, &obj)
+	if err != nil {
+		return
+	}
+
+	user = obj.Data
+	return
 }
 
 // Me returns a *Me with informations about the current logged in account
 func (r *Reddit) Me() (me *Me, err error) {
-  request,err := r.Request("GET","/api/v1/me", nil)
-  if err != nil {
-    return
-  }
+	request, err := r.Request("GET", "/api/v1/me", nil)
+	if err != nil {
+		return
+	}
 
-  data,err := r.JsonResponse(request)
-  if err != nil {
-    return
-  }
+	data, err := r.JsonResponse(request)
+	if err != nil {
+		return
+	}
 
-  err = json.Unmarshal(data, &me)
-  return
+	err = json.Unmarshal(data, &me)
+	return
 }
